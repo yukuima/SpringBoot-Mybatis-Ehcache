53d335255da1252bd81ebd888a50f6610cd134be
yukuima <58396683@qq.com>
2019-06-05 20:55:43 +0800

    commit

---
+++ src/main/java/com/myk/learning/mybatis/ehcache/config/CacheConfiguration.java
@@ -0,0 +1,19 @@
+package com.myk.learning.mybatis.ehcache.config;
+
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Configuration;
+
+
+/**
+ * Cache configuration
+ * <p/>
+ * Created in 2018.12.12
+ * <p/>
+ *
+ * @author myk
+ */
+@Configuration
+@EnableCaching
+public class CacheConfiguration {
+
+}
